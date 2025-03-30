143d114601eff5d3d308b43e9e89e3501264163f
Lpopivnyak <stadnykroman2010@gmail.com>
2025-03-30 16:27:50 +0300

    filtr.py

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
 from PyQt6.QtWidgets import *
+from PIL import Image,ImageFilter,ImageEnhance
+
+
+import os
+
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import *
+
+from PIL import Image, ImageFilter, ImageEnhance
+
+
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
 
 app = QApplication([])
+
+app.setStyleSheet("""
+        QPushButton
+        {
+            border-style: groove;
+            border-width: 5px;
+            background-color: #735fff;
+            border-color: #7500ff;
+            border-radius: 7px;
+            color: white;
+            }
+            
+        QWidget
+        {
+            background: #9e6fff;
+            }
+        """)
 window = QWidget()
 
 
-photo_lbl = QLabel("1234")
+photo_lbl = QLabel("Фоточка")
 
 photo = QListWidget()
 
@@ -41,15 +83,68 @@ v2.addLayout(h1)
 main_line.addLayout(v1)
 main_line.addLayout(v2)
 
+class ImageProcessor:
+    def __init__ (self):
+        self.folder = ""
+        self.filename = ""
+        self.image = ""
+    def load(self):
+        img_path = os.path.join(self.folder, self.filename)
+        self.image = Image.open(img_path)
+
+    def show(self):
+        pix = pil2pixmap(self.image)
+        pix = pix.scaledToWidth(450)
+        photo_lbl.setPixmap(pix)
+
+    def rotate_left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.show()
+
+    def rotate_right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.show()
+
+    def rotate_mirror(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show()
+
+    def rotate_sharpness(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.show()
+
+    def rotate_ch_b(self):
+        self.image = self.image.convert("L")
+        self.show()
+
+
+ip = ImageProcessor()
+ip.filename = "contrast.jpg"
+ip.load()
+ip.show()
+
+def open_folder():
+    ip.folder = QFileDialog.getExistingDirectory()
+    files = os.listdir(ip.folder)
+    photo.clear()
+    for file in files:
+        if file.endswith(".jpg"):
+            photo.addItem(file)
+
+def show_chosen_image():
+    ip.filename = photo.currentItem().text()
+    ip.load()
+    ip.show()
+
+photo.currentRowChanged.connect(show_chosen_image)
+file_btn.clicked.connect(open_folder)
+left_btn.clicked.connect(ip.rotate_left)
+right_btn.clicked.connect(ip.rotate_right)
+mirror_btn.clicked.connect(ip.rotate_mirror)
+sharpness_btn.clicked.connect(ip.rotate_sharpness)
+ch_b_btn.clicked.connect(ip.rotate_ch_b)
 window.setLayout(main_line)
 window.show()
 app.exec()
 
 
-app.setStyleSheet("""
-        QPushButton
-        {
-            border-style: groove;
-            border-width: 5px;
-            }
-        """)
