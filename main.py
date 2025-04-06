6881d82c9cbf9c7f6c3cfaaec1ab3d30bf1663aa
Lpopivnyak <stadnykroman2010@gmail.com>
2025-04-06 16:28:29 +0300

    QPushButton

--- main.py
+++ main.py
@@ -66,6 +66,11 @@ right_btn = QPushButton("Вправо")
 mirror_btn = QPushButton("Дзеркало")
 sharpness_btn = QPushButton("Різкість")
 ch_b_btn = QPushButton("Ч/Б")
+contour_btn = QPushButton("Контур")
+contrast_btn = QPushButton("Контраст 0.5")
+contrast_1_btn = QPushButton("Контраст 1.5")
+rids_btn = QPushButton("Покращення ребер")
+back_btn = QPushButton("Початкове фото")
 
 v2 =QVBoxLayout()
 v2.addWidget(photo_lbl)
@@ -78,6 +83,11 @@ h1.addWidget(right_btn)
 h1.addWidget(mirror_btn)
 h1.addWidget(sharpness_btn)
 h1.addWidget(ch_b_btn)
+h1.addWidget(contour_btn)
+h1.addWidget(contrast_btn)
+h1.addWidget(contrast_1_btn)
+h1.addWidget(rids_btn)
+h1.addWidget(back_btn)
 v2.addLayout(h1)
 
 main_line.addLayout(v1)
@@ -117,6 +127,26 @@ class ImageProcessor:
         self.image = self.image.convert("L")
         self.show()
 
+    def rotate_contour(self):
+        self.image = self.image.filter(ImageFilter.CONTOUR)
+        self.show()
+
+    def rotate_contrast(self):
+        self.image = ImageEnhance.Contrast(self.image).enhance(0.5)
+        self.show()
+
+    def rotate_1_contrast(self):
+        self.image = ImageEnhance.Contrast(self.image).enhance(1.5)
+        self.show()
+
+    def rotate_rids(self):
+        self.image = ImageEnhance.Contrast(self.image).enhance(1.5)
+        self.show()
+
+    def rotate_back(self):
+        self.load()
+        self.show()
+
 
 ip = ImageProcessor()
 ip.filename = "contrast.jpg"
@@ -143,6 +173,11 @@ right_btn.clicked.connect(ip.rotate_right)
 mirror_btn.clicked.connect(ip.rotate_mirror)
 sharpness_btn.clicked.connect(ip.rotate_sharpness)
 ch_b_btn.clicked.connect(ip.rotate_ch_b)
+contour_btn.clicked.connect(ip.rotate_contour)
+contrast_btn.clicked.connect(ip.rotate_contrast)
+contrast_1_btn.clicked.connect(ip.rotate_1_contrast)
+rids_btn.clicked.connect(ip.rotate_rids)
+back_btn.clicked.connect(ip.rotate_back)
 window.setLayout(main_line)
 window.show()
 app.exec()
